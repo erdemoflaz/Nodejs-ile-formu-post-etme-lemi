250134c13fa412d26bb92fb81979cbae307d49af
erdemoflaz <erdemoflaz4@gmail.com>
2016-08-03 23:54:09 +0300

    files

---
+++ server.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var fs = require('fs');
+var formidable = require("formidable");
+var util = require('util');
+
+var server = http.createServer(function (req, res) {
+    if (req.method.toLowerCase() == 'get') {
+        displayForm(res);
+    } else if (req.method.toLowerCase() == 'post') {
+        processAllFieldsOfTheForm(req, res);
+    }
+
+});
+
+function displayForm(res) {
+    fs.readFile('form.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+                'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+}
+
+function processAllFieldsOfTheForm(req, res) {
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, veriler) {
+        //Store the data from the fields in your data store.
+        //The data store could be a file or database or any other store based
+        //on your application.
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+        res.write('post ile gelenleri goster :\n\n');
+        res.end(util.inspect({
+            veriler: veriler,
+        }));
+    });
+}
+
+server.listen(1185);
+console.log("server listening on 1185");
